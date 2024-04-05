836e05ee7225ff95fedf41c4cb13dbd4a7aa2c51
Berat Demir <berat.demir@commencis.com>
2024-04-05 23:21:14 +0300

    Add xor value to SecretDeclaration

--- Sources/SecretsVaultModel/SecretModels/SecretDeclaration.swift
+++ Sources/SecretsVaultModel/SecretModels/SecretDeclaration.swift
@@ -4,11 +4,13 @@ import SecretsVaultUtility
 public struct SecretDeclaration: Codable, Sendable {
 
     public let secretName: String
+    public let xorValue: UInt8?
     public let secrets: [SecretItem]
     public let strict: Bool
 
-    public init(secretName: String, secrets: [SecretItem], strict: Bool) {
+    public init(secretName: String, secrets: [SecretItem], xorValue: UInt8?, strict: Bool) {
         self.secretName = secretName
+        self.xorValue = xorValue
         self.secrets = secrets
         self.strict = strict
     }
@@ -16,6 +18,7 @@ public struct SecretDeclaration: Codable, Sendable {
     public init(from decoder: Decoder) throws {
         let container = try decoder.container(keyedBy: CodingKeys.self)
         self.secretName = try container.decode(String.self, forKey: .secretName)
+        self.xorValue = try container.decodeIfPresent(UInt8.self, forKey: .xorValue)
         self.secrets = try container.decode([SecretItem].self, forKey: .secrets)
         self.strict = try container.decodeIfPresent(Bool.self, forKey: .strict).orTrue
     }
